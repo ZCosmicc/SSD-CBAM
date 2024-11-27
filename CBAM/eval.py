6334639375a36d8230f3741d83efd4028252f5b2
Fariz Fadillah <101439750+ZCosmicc@users.noreply.github.com>
2024-11-27 15:37:12 +0800

    Update eval.py
    Updating checkpoint path

--- CBAM/eval.py
+++ CBAM/eval.py
@@ -12,7 +12,7 @@ keep_difficult = True  # difficult ground truth objects must always be considere
 batch_size = 16
 workers = 4
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-checkpoint = '/kaggle/working/SSD-CBAM/results/checkpoint_epoch_99.pth.tar'
+checkpoint = '/SSD-CBAM/results/checkpoint_epoch_99.pth.tar'
 
 # Load model checkpoint that is to be evaluated
 checkpoint = torch.load(checkpoint, weights_only=False)
