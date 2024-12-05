a1f37d0a2bb7798058deb156ce8a954915ec23e2
Fariz Fadillah <101439750+ZCosmicc@users.noreply.github.com>
2024-12-05 20:00:55 +0800

    Update model.py
    Incorporates a parameterized CBAM integration for easy toggling / switching on/off the CBAM modules

--- CBAM/model.py
+++ CBAM/model.py
@@ -98,12 +98,13 @@ class CBAM(nn.Module):
             x_out = self.SpatialGate(x_out)
         return x_out
 
+# VGG Base with CBAM toggle
 class VGGBase(nn.Module):
     """
     VGG base convolutions to produce lower-level feature maps.
     """
 
-    def __init__(self):
+    def __init__(self, use_cbam=True):
         super(VGGBase, self).__init__()
 
         # Standard convolutional layers in VGG16
@@ -138,8 +139,10 @@ class VGGBase(nn.Module):
         # Load pretrained layers
         self.load_pretrained_layers()
 
-        self.cbam4_3 = CBAM(512, 16)
-        self.cbam7 = CBAM(1024, 16)
+        self.use_cbam = use_cbam
+        if self.use_cbam:
+            self.cbam4_3 = CBAM(512, 16)
+            self.cbam7 = CBAM(1024, 16)
 
     def forward(self, image):
         """
@@ -165,7 +168,8 @@ class VGGBase(nn.Module):
         out = F.relu(self.conv4_2(out))  # (N, 512, 38, 38)
         out = F.relu(self.conv4_3(out))  # (N, 512, 38, 38)
         conv4_3_feats = out  # (N, 512, 38, 38)
-        conv4_3_feats = self.cbam4_3(conv4_3_feats)
+        if self.use_cbam:
+            conv4_3_feats = self.cbam4_3(conv4_3_feats)
         out = self.pool4(out)  # (N, 512, 19, 19)
 
         out = F.relu(self.conv5_1(out))  # (N, 512, 19, 19)
@@ -176,7 +180,8 @@ class VGGBase(nn.Module):
         out = F.relu(self.conv6(out))  # (N, 1024, 19, 19)
 
         conv7_feats = F.relu(self.conv7(out))  # (N, 1024, 19, 19)
-        conv7_feats = self.cbam7(conv7_feats)
+        if self.use_cbam:
+            conv7_feats = self.cbam7(conv7_feats)
 
         # Lower-level feature maps
         return conv4_3_feats, conv7_feats
@@ -229,25 +234,30 @@ class AuxiliaryConvolutions(nn.Module):
     Additional convolutions to produce higher-level feature maps.
     """
 
-    def __init__(self):
+    def __init__(self, use_cbam=True):
         super(AuxiliaryConvolutions, self).__init__()
 
         # Auxiliary/additional convolutions on top of the VGG base
         self.conv8_1 = nn.Conv2d(1024, 256, kernel_size=1, padding=0)  # stride = 1, by default
         self.conv8_2 = nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1)  # dim. reduction because stride > 1
-        self.cbam8_2 = CBAM(512, 16)
+        self.use_cbam = use_cbam
+        if self.use_cbam:
+            self.cbam8_2 = CBAM(512, 16)
 
         self.conv9_1 = nn.Conv2d(512, 128, kernel_size=1, padding=0)
         self.conv9_2 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)  # dim. reduction because stride > 1
-        self.cbam9_2 = CBAM(256, 16)
+        if self.use_cbam:
+            self.cbam9_2 = CBAM(256, 16)
 
         self.conv10_1 = nn.Conv2d(256, 128, kernel_size=1, padding=0)
         self.conv10_2 = nn.Conv2d(128, 256, kernel_size=3, padding=0)  # dim. reduction because padding = 0
-        self.cbam10_2 = CBAM(256, 16)
+        if self.use_cbam:
+            self.cbam10_2 = CBAM(256, 16)
 
         self.conv11_1 = nn.Conv2d(256, 128, kernel_size=1, padding=0)
         self.conv11_2 = nn.Conv2d(128, 256, kernel_size=3, padding=0)  # dim. reduction because padding = 0
-        self.cbam11_2 = CBAM(256, 16)
+        if self.use_cbam:
+            self.cbam11_2 = CBAM(256, 16)
 
         # Initialize convolutions' parameters
         self.init_conv2d()
@@ -271,21 +281,25 @@ class AuxiliaryConvolutions(nn.Module):
         out = F.relu(self.conv8_1(conv7_feats))  # (N, 256, 19, 19)
         out = F.relu(self.conv8_2(out))  # (N, 512, 10, 10)
         conv8_2_feats = out  # (N, 512, 10, 10)
-        conv8_2_feats = self.cbam8_2(conv8_2_feats)
+        if self.use_cbam:
+            conv8_2_feats = self.cbam8_2(conv8_2_feats)
 
         out = F.relu(self.conv9_1(out))  # (N, 128, 10, 10)
         out = F.relu(self.conv9_2(out))  # (N, 256, 5, 5)
         conv9_2_feats = out  # (N, 256, 5, 5)
-        conv9_2_feats = self.cbam9_2(conv9_2_feats)
+        if self.use_cbam:
+            conv9_2_feats = self.cbam9_2(conv9_2_feats)
 
         out = F.relu(self.conv10_1(out))  # (N, 128, 5, 5)
         out = F.relu(self.conv10_2(out))  # (N, 256, 3, 3)
         conv10_2_feats = out  # (N, 256, 3, 3)
-        conv10_2_feats = self.cbam10_2(conv10_2_feats)
-
+        if self.use_cbam:
+            conv10_2_feats = self.cbam10_2(conv10_2_feats)
+            
         out = F.relu(self.conv11_1(out))  # (N, 128, 3, 3)
         conv11_2_feats = F.relu(self.conv11_2(out))  # (N, 256, 1, 1)
-        conv11_2_feats = self.cbam11_2(conv11_2_feats)
+        if self.use_cbam:
+            conv11_2_feats = self.cbam11_2(conv11_2_feats)
 
         # Higher-level feature maps
         return conv8_2_feats, conv9_2_feats, conv10_2_feats, conv11_2_feats
@@ -430,13 +444,13 @@ class SSD300(nn.Module):
     The SSD300 network - encapsulates the base VGG network, auxiliary, and prediction convolutions.
     """
 
-    def __init__(self, n_classes):
+    def __init__(self, n_classes, use_cbam=True):
         super(SSD300, self).__init__()
 
         self.n_classes = n_classes
 
-        self.base = VGGBase()
-        self.aux_convs = AuxiliaryConvolutions()
+        self.base = VGGBase(use_cbam=use_cbam)
+        self.aux_convs = AuxiliaryConvolutions(use_cbam=use_cbam)
         self.pred_convs = PredictionConvolutions(n_classes)
 
         # Since lower level features (conv4_3_feats) have considerably larger scales, we take the L2 norm and rescale
