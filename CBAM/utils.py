2c4ff4c136485151578b23c3b3014a49afb45f30
Fariz Fadillah <101439750+ZCosmicc@users.noreply.github.com>
2025-04-28 08:36:10 +0800

    Update utils.py
    Color mapping for VOC experiment

--- CBAM/utils.py
+++ CBAM/utils.py
@@ -26,10 +26,19 @@ voc_labels = (
 label_map = {k: v for v, k in enumerate(voc_labels)}
 rev_label_map = {v: k for k, v in label_map.items()}
 
+# Color map for bounding boxes of detected objects
+distinct_colors = [
+    '#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', '#911eb4', '#46f0f0',
+    '#f032e6', '#d2f53c', '#fabebe', '#008080', '#e6beff', '#aa6e28', '#fffac8',
+    '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000080', '#808080', '#FFFFFF'
+]
+
+label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
+
 
 # Color map for bounding boxes of detected objects from https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
-distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231']
-label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
+#distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231']
+#label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
 
 
 def parse_annotation(annotation_path):
