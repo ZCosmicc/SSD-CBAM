230187caf8144452f68f601e4836640d91801a85
Fariz Fadillah <101439750+ZCosmicc@users.noreply.github.com>
2025-04-30 12:52:28 +0800

    Update datasets.py
    Adding support to handle K-Fold

--- CBAM/datasets.py
+++ CBAM/datasets.py
@@ -9,47 +9,51 @@ from utils import transform
 class PascalVOCDataset(Dataset):
     """
     A PyTorch Dataset class to be used in a PyTorch DataLoader to create batches.
+    Supports optional indexing for K-Fold Cross-Validation.
     """
 
-    def __init__(self, data_folder, split, keep_difficult=False):
+    def __init__(self, data_folder, split, keep_difficult=False, indices=None):
         """
         :param data_folder: folder where data files are stored
         :param split: split, one of 'TRAIN' or 'TEST'
         :param keep_difficult: keep or discard objects that are considered difficult to detect?
+        :param indices: optional list of indices to subset the dataset (for k-fold)
         """
         self.split = split.upper()
-
         assert self.split in {'TRAIN', 'TEST'}
 
         self.data_folder = data_folder
         self.keep_difficult = keep_difficult
 
-        # Read data files
+        # Load full data
         with open(os.path.join(data_folder, self.split + '_images.json'), 'r') as j:
-            self.images = json.load(j)
+            all_images = json.load(j)
         with open(os.path.join(data_folder, self.split + '_objects.json'), 'r') as j:
-            self.objects = json.load(j)
+            all_objects = json.load(j)
 
-        assert len(self.images) == len(self.objects)
+        assert len(all_images) == len(all_objects)
+
+        # Apply index filtering if provided
+        if indices is not None:
+            self.images = [all_images[i] for i in indices]
+            self.objects = [all_objects[i] for i in indices]
+        else:
+            self.images = all_images
+            self.objects = all_objects
 
     def __getitem__(self, i):
-        # Read image
-        image = Image.open(self.images[i], mode='r')
-        image = image.convert('RGB')
+        image = Image.open(self.images[i], mode='r').convert('RGB')
 
-        # Read objects in this image (bounding boxes, labels, difficulties)
         objects = self.objects[i]
         boxes = torch.FloatTensor(objects['boxes'])  # (n_objects, 4)
         labels = torch.LongTensor(objects['labels'])  # (n_objects)
         difficulties = torch.ByteTensor(objects['difficulties'])  # (n_objects)
 
-        # Discard difficult objects, if desired
         if not self.keep_difficult:
             boxes = boxes[1 - difficulties]
             labels = labels[1 - difficulties]
             difficulties = difficulties[1 - difficulties]
 
-        # Apply transformations
         image, boxes, labels, difficulties = transform(image, boxes, labels, difficulties, split=self.split)
 
         return image, boxes, labels, difficulties
@@ -58,17 +62,6 @@ class PascalVOCDataset(Dataset):
         return len(self.images)
 
     def collate_fn(self, batch):
-        """
-        Since each image may have a different number of objects, we need a collate function (to be passed to the DataLoader).
-
-        This describes how to combine these tensors of different sizes. We use lists.
-
-        Note: this need not be defined in this Class, can be standalone.
-
-        :param batch: an iterable of N sets from __getitem__()
-        :return: a tensor of images, lists of varying-size tensors of bounding boxes, labels, and difficulties
-        """
-
         images = list()
         boxes = list()
         labels = list()
